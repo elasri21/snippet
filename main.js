8a2fcd5502762ae307a5e1b1323b437596863ac1
elasri21 <islame.edu@gmail.com>
2022-08-02 22:27:00 +0100

    files were added

---
+++ main.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+const closeMenu = document.querySelector(".close");
+
+menu.addEventListener("click", function() {
+    this.style.display = "none";
+    nav.style.display = "block";
+    closeMenu.style.display = "block";
+});
+closeMenu.addEventListener("click", function() {
+    this.style.display = "none";
+    nav.style.display = "none";
+    menu.style.display = "block";
+});
